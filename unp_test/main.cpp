dc9e6c6df294c74679e15235ed669018df6625ce
mrzhuzhe <466648706@qq.com>
2024-05-01 16:24:13 +0800

    move unp test

---
+++ unp_test/main.cpp
@@ -0,0 +1,13 @@
+/*
+    Todo
+    1. gun auto config
+    2. unp header 
+    3. header implement is still missing
+*/   
+#include <iostream>
+#include "inc/unp.h"
+
+int main(){
+    printf("unp test go go go \n");  
+    return 0;
+}
