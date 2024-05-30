a83e93bbb6f8b2963ed2e13e9b2c36018a2cc5e1
mrzhuzhe <466648706@qq.com>
2024-05-30 23:31:13 +0800

    test

--- kernel_test/chardev.h
+++ kernel_test/chardev.h
@@ -7,8 +7,16 @@ https://tldp.org/LDP/lkmpg/2.6/html/x569.html
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 
+int init_module(void);
+void cleanup_module(void);
+int device_open(struct inode *, struct file *);
+int device_release(struct inode *, struct file *);
+ssize_t device_read(struct file *, char *, size_t, loff_t *);
+ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
-
+#define SUCCESS 0
+#define DEVICE_NAME "chardev"
+#define BUF_LEN 80
 
 
 
