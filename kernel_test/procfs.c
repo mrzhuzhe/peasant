f289553fbea14a79c1059c9c9be6c2c1674b7b5b
mrzhuzhe <466648706@qq.com>
2024-06-04 22:52:06 +0800

    proc write

--- kernel_test/procfs.c
+++ kernel_test/procfs.c
@@ -9,50 +9,23 @@ a better version
 #include <linux/uaccess.h>
 #include <linux/version.h>
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+#include <linux/minmax.h>
+#endif
+
 #define procfs_name "hellofatass"
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
 #define HAVE_PROC_OPS
 #endif
 
-/* Put data into the proc fs file.
- * 
- * Arguments
- * =========
- * 1. The buffer where the data is to be inserted, if
- *    you decide to use it.
- * 2. A pointer to a pointer to characters. This is
- *    useful if you don't want to use the buffer
- *    allocated by the kernel.
- * 3. The current position in the file
- * 4. The size of the buffer in the first argument.
- * 5. Write a "1" here to indicate EOF.
- * 6. A pointer to data (useful in case one common 
- *    read for multiple /proc/... entries)
- *
- * Usage and Return Value
- * ======================
- * A return value of zero means you have no further
- * information at this time (end of file). A negative
- * return value is an error condition.
- *
- * For More Information
- * ====================
- * The way I discovered what to do with this function
- * wasn't by reading documentation, but by reading the
- * code which used it. I just looked to see what uses
- * the get_info field of proc_dir_entry struct (I used a
- * combination of find and grep, if you're interested),
- * and I saw that  it is used in <kernel source
- * directory>/fs/proc/array.c.
- *
- * If something is unknown about the kernel, this is
- * usually the way to go. In Linux we have the great
- * advantage of having the kernel source code for
- * free - use it.
- */
+#define PROCFS_MAX_SIZE 2048UL
+#define PROCFS_ENTRY_FILENAME "buffer2k"
 
 struct proc_dir_entry *Our_Proc_File; 
+char procfs_buffer[PROCFS_MAX_SIZE];
+unsigned long procfs_buffer_size = 0;
+
 
 ssize_t procfile_read(struct file *file_pointer, char __user *buffer, size_t buffer_length, loff_t *offset)
 {
@@ -68,6 +41,9 @@ ssize_t procfile_read(struct file *file_pointer, char __user *buffer, size_t buf
 	 * that it has no more information, or until its
 	 * buffer is filled.
 	 */
+	if(*offset || procfs_buffer_size == 0){
+	
+	}
 	char s[] = "Hello mfs\n";
 	int len = sizeof(s);
 	ssize_t ret = len;
