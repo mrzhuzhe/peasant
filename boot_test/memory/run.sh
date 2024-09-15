cd712d9b985afef82997fa9f77467294e401f87f
mrzhuzhe <466648706@qq.com>
2024-09-15 23:30:11 +0800

    org

--- boot_test/memory/run.sh
+++ boot_test/memory/run.sh
@@ -1,3 +1,6 @@
 nasm -f bin boot_sect_memory.asm -o boot_sect_memory.bin
 
+nasm -f bin boot_sect_memory_org.asm -o boot_sect_memory_org.bin
+
 # qemu-system-x86_64 boot_sect_memory.bin
+# qemu-system-x86_64 boot_sect_memory_org.bin
