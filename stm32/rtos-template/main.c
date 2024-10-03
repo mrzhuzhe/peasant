db386d1ab5665184704fbefc0c7c45e4c99eecc6
mrzhuzhe <466648706@qq.com>
2024-10-03 16:04:25 +0800

    okay

--- stm32/rtos-template/main.c
+++ stm32/rtos-template/main.c
@@ -3,19 +3,25 @@
  * The LED on PC13 is toggled in task1.
  */
 #include "FreeRTOS.h"
+#include "portmacro.h"
 #include "task.h"
+#include "list.h"
+#include "opencm3.h"
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include "opencm3.h"
 
-extern void vApplicationStackOverflowHook(TaskHandle_t pxTask, portCHAR *pcTaskName);
+
+// extern void vApplicationStackOverflowHook(TaskHandle_t pxTask, portCHAR *pcTaskName);
 
 void
-vApplicationStackOverflowHook(TaskHandle_t pxTask, portCHAR *pcTaskName) {
-	(void)pxTask;
+vApplicationStackOverflowHook(TaskHandle_t xTask, portCHAR *pcTaskName) {
+	(void)xTask;
 	(void)pcTaskName;
-	for(;;);
+	for(;;){
+		// gpio_toggle(GPIOC,GPIO14);
+		// vTaskDelay(pdMS_TO_TICKS(500));
+	};
 }
 
 static void
@@ -25,19 +31,27 @@ task1(void *args) {
 	(void)args;
 
 	for (;;) {
-		gpio_clear(GPIOC,GPIO14);
-		for (i = 0; i < 5e5; i++)
-			__asm__("nop");
-		gpio_set(GPIOC,GPIO14);
+		// gpio_clear(GPIOC,GPIO14);
+		// for (i = 0; i < 5e5; i++)
+		// 	__asm__("nop");		
+		// //vTaskDelay(pdMS_TO_TICKS(500));
+		// gpio_set(GPIOC,GPIO14);
+		// for (i = 0; i < 5e5; i++)
+		// 	__asm__("nop");	
+		gpio_toggle(GPIOC,GPIO14);
+		vTaskDelay(pdMS_TO_TICKS(500));
 	}
 }
 
 int
 main(void) {
 
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Use this for "blue pill"
+	//rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Use this for "blue pill"
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO14);
+	gpio_set(GPIOC,GPIO14);
 
 	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
 	vTaskStartScheduler();
