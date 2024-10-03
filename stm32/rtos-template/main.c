56503c25a47ea522684670adb923f64259ebcaa1
mrzhuzhe <466648706@qq.com>
2024-10-03 15:24:40 +0800

    format

--- stm32/rtos-template/main.c
+++ stm32/rtos-template/main.c
@@ -7,6 +7,7 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+#include "opencm3.h"
 
 extern void vApplicationStackOverflowHook(TaskHandle_t pxTask, portCHAR *pcTaskName);
 
@@ -24,9 +25,10 @@ task1(void *args) {
 	(void)args;
 
 	for (;;) {
-		gpio_toggle(GPIOC,GPIO13);
-		for (i = 0; i < 1000000; i++)
+		gpio_clear(GPIOC,GPIO14);
+		for (i = 0; i < 5e5; i++)
 			__asm__("nop");
+		gpio_set(GPIOC,GPIO14);
 	}
 }
 
@@ -35,7 +37,7 @@ main(void) {
 
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Use this for "blue pill"
 	rcc_periph_clock_enable(RCC_GPIOC);
-	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO13);
+	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO14);
 
 	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
 	vTaskStartScheduler();
