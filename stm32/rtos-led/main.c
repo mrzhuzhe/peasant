3528bdcfe636dd2bd33a545730c11a226c9a35e1
mrzhuzhe <466648706@qq.com>
2024-10-13 22:48:00 +0800

    i2c is ok

--- stm32/rtos-led/main.c
+++ stm32/rtos-led/main.c
@@ -354,7 +354,11 @@ main(void) {
 	OLED_Init();
 
 	OLED_ShowChar(1, 1, 'A');
-
+	OLED_ShowString(1, 3, "Fuck you!");
+	OLED_ShowNum(2, 1, 12345, 5);
+	OLED_ShowSignedNum(2, 7, -66, 4);
+	OLED_ShowHexNum(3, 1, 0xAA55, 4);
+	OLED_ShowBinNum(4, 1, 0xAA55, 16);
 	// xTaskCreate(monitor_task,"monitor",500,NULL,1,NULL);
 	// vTaskStartScheduler();
 	for (;;);
