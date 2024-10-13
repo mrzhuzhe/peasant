1b10cf5c0a35a5e50c3cddc0a2c8a3dd2b52df46
mrzhuzhe <466648706@qq.com>
2024-10-13 22:45:33 +0800

    c ok

--- stm32/rtos-led/main.c
+++ stm32/rtos-led/main.c
@@ -20,221 +20,343 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "mcuio.h"
-#include "miniprintf.h"
-#include "intelhex.h"
-
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
-#include "meter.h"
-#include "oled.h"
+#include "OLED_Font.h"
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
 #include "opencm3.h"
 
-void
-oled_command(uint8_t byte) {
-	gpio_clear(GPIOB,GPIO10);
-	spi_enable(SPI1);
-	spi_xfer(SPI1,byte);
-	spi_disable(SPI1);
+// #define OLED_W_SCL(x)		GPIO_WriteBit(GPIOB, GPIO_Pin_8, (BitAction)(x))
+// #define OLED_W_SDA(x)		GPIO_WriteBit(GPIOB, GPIO_Pin_9, (BitAction)(x))
+/*引脚初始化*/
+void OLED_I2C_Init(void)
+{
+		rcc_periph_clock_enable(RCC_GPIOB);
+
+	// LED on PC13
+	gpio_set_mode(GPIOB,
+		GPIO_MODE_OUTPUT_50_MHZ,
+		GPIO_CNF_OUTPUT_OPENDRAIN,
+		GPIO8|GPIO9);
+		
+	gpio_set(GPIOB, GPIO8); //	OLED_W_SCL (1);
+	gpio_set(GPIOB, GPIO9);	//	OLED_W_SDA (1);
 }
 
-void
-oled_command2(uint8_t byte,uint8_t byte2) {
-	gpio_clear(GPIOB,GPIO10);
-	spi_enable(SPI1);
-	spi_xfer(SPI1,byte);
-	spi_xfer(SPI1,byte2);
-	spi_disable(SPI1);
+void OLED_I2C_Start(void)
+{
+	gpio_set(GPIOB, GPIO9);
+	gpio_set(GPIOB, GPIO8);
+	gpio_clear(GPIOB, GPIO9);
+	gpio_clear(GPIOB, GPIO8);
 }
 
-void
-oled_data(uint8_t byte) {
-	gpio_set(GPIOB,GPIO10);
-	spi_enable(SPI1);
-	spi_xfer(SPI1,byte);
-	spi_disable(SPI1);
+/**
+  * @brief  I2C停止
+  * @param  无
+  * @retval 无
+  */
+void OLED_I2C_Stop(void)
+{
+	gpio_clear(GPIOB, GPIO9);
+	gpio_set(GPIOB, GPIO8);
+	gpio_set(GPIOB, GPIO9);
 }
 
-static void
-oled_reset(void) {
-	gpio_clear(GPIOB,GPIO11);
-	vTaskDelay(1);
-	gpio_set(GPIOB,GPIO11);
-}
-
-static void
-oled_init(void) {
-	static uint8_t cmds[] = {
-		0xAE, 0x00, 0x10, 0x40, 0x81, 0xCF, 0xA1, 0xA6, 
-		0xA8, 0x3F, 0xD3, 0x00, 0xD5, 0x80, 0xD9, 0xF1, 
-		0xDA, 0x12, 0xDB, 0x40, 0x8D, 0x14, 0xAF, 0xFF };
-
-	gpio_clear(GPIOC,GPIO13);
-	oled_reset();
-	for ( unsigned ux=0; cmds[ux] != 0xFF; ++ux )
-		oled_command(cmds[ux]);
-	gpio_set(GPIOC,GPIO13);
-}
-
-/*
- * Monitor task:
- */
-static void
-monitor_task(void *arg __attribute((unused))) {
-	struct Meter m1;
-	bool menuf = true;
-	float v = 1.3;
-	char ch;
-	
-	std_printf("\nMonitor Task Started.\n");
-
-	oled_init();
-	meter_init(&m1,3.5);
-
-	meter_set_value(&m1,v);
-	meter_update();
-
-	for (;;) {
-		if ( menuf ) {
-			std_printf(
-				"\nTest Menu:\n"
-				"  0 .. set to 0.0 volts\n"
-				"  1 .. set to 1.0 volts\n"
-				"  2 .. set to 2.0 volts\n"
-				"  3 .. set to 3.0 volts\n"
-				"  4 .. set to 3.5 volts\n"
-				"  + .. increase by 0.1 volts\n"
-				"  - .. decrease by 0.1 volts\n"
-			);
+/**
+  * @brief  I2C发送一个字节
+  * @param  Byte 要发送的一个字节
+  * @retval 无
+  */
+void OLED_I2C_SendByte(uint8_t Byte)
+{
+	uint8_t i;
+	for (i = 0; i < 8; i++)
+	{
+		//OLED_W_SDA(Byte & (0x80 >> i));
+		if (Byte & (0x80 >> i)) {
+			gpio_set(GPIOB, GPIO9);	
+		} else {
+			gpio_clear(GPIOB, GPIO9);	
 		}
-		menuf = false;
+		gpio_set(GPIOB, GPIO8);
+		gpio_clear(GPIOB, GPIO8);
+	}
+	gpio_set(GPIOB, GPIO8);	//额外的一个时钟，不处理应答信号
+	gpio_clear(GPIOB, GPIO8);
+}
 
-		std_printf("\n: ");
-		ch = std_getc();
+/**
+  * @brief  OLED写命令
+  * @param  Command 要写入的命令
+  * @retval 无
+  */
+void OLED_WriteCommand(uint8_t Command)
+{
+	OLED_I2C_Start();
+	OLED_I2C_SendByte(0x78);		//从机地址
+	OLED_I2C_SendByte(0x00);		//写命令
+	OLED_I2C_SendByte(Command); 
+	OLED_I2C_Stop();
+}
 
-		if ( isalpha(ch) )
-			ch = toupper(ch);
-		std_printf("%c\n",ch);
+/**
+  * @brief  OLED写数据
+  * @param  Data 要写入的数据
+  * @retval 无
+  */
+void OLED_WriteData(uint8_t Data)
+{
+	OLED_I2C_Start();
+	OLED_I2C_SendByte(0x78);		//从机地址
+	OLED_I2C_SendByte(0x40);		//写数据
+	OLED_I2C_SendByte(Data);
+	OLED_I2C_Stop();
+}
 
-		switch ( ch ) {
-		case '?':
-		case '\r':
-		case '\n':
-			menuf = true;
-			break;		
-		case '+':
-			v += 0.1;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		case '-':
-			v -= 0.1;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		case '0':
-			v = 0.0;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		case '1':
-			v = 1.0;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		case '2':
-			v = 2.0;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		case '3':
-			v = 3.0;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		case '4':
-			v = 3.5;
-			meter_set_value(&m1,v);
-			meter_update();
-			break;
-		default:
-			std_printf(" ???\n");
-			menuf = true;
+/**
+  * @brief  OLED设置光标位置
+  * @param  Y 以左上角为原点，向下方向的坐标，范围：0~7
+  * @param  X 以左上角为原点，向右方向的坐标，范围：0~127
+  * @retval 无
+  */
+void OLED_SetCursor(uint8_t Y, uint8_t X)
+{
+	OLED_WriteCommand(0xB0 | Y);					//设置Y位置
+	OLED_WriteCommand(0x10 | ((X & 0xF0) >> 4));	//设置X位置高4位
+	OLED_WriteCommand(0x00 | (X & 0x0F));			//设置X位置低4位
+}
+
+/**
+  * @brief  OLED清屏
+  * @param  无
+  * @retval 无
+  */
+void OLED_Clear(void)
+{  
+	uint8_t i, j;
+	for (j = 0; j < 8; j++)
+	{
+		OLED_SetCursor(j, 0);
+		for(i = 0; i < 128; i++)
+		{
+			OLED_WriteData(0x00);
 		}
 	}
+}
+
+/**
+  * @brief  OLED显示一个字符
+  * @param  Line 行位置，范围：1~4
+  * @param  Column 列位置，范围：1~16
+  * @param  Char 要显示的一个字符，范围：ASCII可见字符
+  * @retval 无
+  */
+void OLED_ShowChar(uint8_t Line, uint8_t Column, char Char)
+{      	
+	uint8_t i;
+	OLED_SetCursor((Line - 1) * 2, (Column - 1) * 8);		//设置光标位置在上半部分
+	for (i = 0; i < 8; i++)
+	{
+		OLED_WriteData(OLED_F8x16[Char - ' '][i]);			//显示上半部分内容
+	}
+	OLED_SetCursor((Line - 1) * 2 + 1, (Column - 1) * 8);	//设置光标位置在下半部分
+	for (i = 0; i < 8; i++)
+	{
+		OLED_WriteData(OLED_F8x16[Char - ' '][i + 8]);		//显示下半部分内容
+	}
+}
+
+/**
+  * @brief  OLED显示字符串
+  * @param  Line 起始行位置，范围：1~4
+  * @param  Column 起始列位置，范围：1~16
+  * @param  String 要显示的字符串，范围：ASCII可见字符
+  * @retval 无
+  */
+void OLED_ShowString(uint8_t Line, uint8_t Column, char *String)
+{
+	uint8_t i;
+	for (i = 0; String[i] != '\0'; i++)
+	{
+		OLED_ShowChar(Line, Column + i, String[i]);
+	}
+}
+
+/**
+  * @brief  OLED次方函数
+  * @retval 返回值等于X的Y次方
+  */
+uint32_t OLED_Pow(uint32_t X, uint32_t Y)
+{
+	uint32_t Result = 1;
+	while (Y--)
+	{
+		Result *= X;
+	}
+	return Result;
+}
+
+/**
+  * @brief  OLED显示数字（十进制，正数）
+  * @param  Line 起始行位置，范围：1~4
+  * @param  Column 起始列位置，范围：1~16
+  * @param  Number 要显示的数字，范围：0~4294967295
+  * @param  Length 要显示数字的长度，范围：1~10
+  * @retval 无
+  */
+void OLED_ShowNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length)
+{
+	uint8_t i;
+	for (i = 0; i < Length; i++)							
+	{
+		OLED_ShowChar(Line, Column + i, Number / OLED_Pow(10, Length - i - 1) % 10 + '0');
+	}
+}
+
+/**
+  * @brief  OLED显示数字（十进制，带符号数）
+  * @param  Line 起始行位置，范围：1~4
+  * @param  Column 起始列位置，范围：1~16
+  * @param  Number 要显示的数字，范围：-2147483648~2147483647
+  * @param  Length 要显示数字的长度，范围：1~10
+  * @retval 无
+  */
+void OLED_ShowSignedNum(uint8_t Line, uint8_t Column, int32_t Number, uint8_t Length)
+{
+	uint8_t i;
+	uint32_t Number1;
+	if (Number >= 0)
+	{
+		OLED_ShowChar(Line, Column, '+');
+		Number1 = Number;
+	}
+	else
+	{
+		OLED_ShowChar(Line, Column, '-');
+		Number1 = -Number;
+	}
+	for (i = 0; i < Length; i++)							
+	{
+		OLED_ShowChar(Line, Column + i + 1, Number1 / OLED_Pow(10, Length - i - 1) % 10 + '0');
+	}
+}
+
+/**
+  * @brief  OLED显示数字（十六进制，正数）
+  * @param  Line 起始行位置，范围：1~4
+  * @param  Column 起始列位置，范围：1~16
+  * @param  Number 要显示的数字，范围：0~0xFFFFFFFF
+  * @param  Length 要显示数字的长度，范围：1~8
+  * @retval 无
+  */
+void OLED_ShowHexNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length)
+{
+	uint8_t i, SingleNumber;
+	for (i = 0; i < Length; i++)							
+	{
+		SingleNumber = Number / OLED_Pow(16, Length - i - 1) % 16;
+		if (SingleNumber < 10)
+		{
+			OLED_ShowChar(Line, Column + i, SingleNumber + '0');
+		}
+		else
+		{
+			OLED_ShowChar(Line, Column + i, SingleNumber - 10 + 'A');
+		}
+	}
+}
+
+/**
+  * @brief  OLED显示数字（二进制，正数）
+  * @param  Line 起始行位置，范围：1~4
+  * @param  Column 起始列位置，范围：1~16
+  * @param  Number 要显示的数字，范围：0~1111 1111 1111 1111
+  * @param  Length 要显示数字的长度，范围：1~16
+  * @retval 无
+  */
+void OLED_ShowBinNum(uint8_t Line, uint8_t Column, uint32_t Number, uint8_t Length)
+{
+	uint8_t i;
+	for (i = 0; i < Length; i++)							
+	{
+		OLED_ShowChar(Line, Column + i, Number / OLED_Pow(2, Length - i - 1) % 2 + '0');
+	}
+}
+
+/**
+  * @brief  OLED初始化
+  * @param  无
+  * @retval 无
+  */
+void OLED_Init(void)
+{
+	uint32_t i, j;
+	
+	for (i = 0; i < 1000; i++)			//上电延时
+	{
+		for (j = 0; j < 1000; j++);
+	}
+	
+	OLED_I2C_Init();			//端口初始化
+	
+	OLED_WriteCommand(0xAE);	//关闭显示
+	
+	OLED_WriteCommand(0xD5);	//设置显示时钟分频比/振荡器频率
+	OLED_WriteCommand(0x80);
+	
+	OLED_WriteCommand(0xA8);	//设置多路复用率
+	OLED_WriteCommand(0x3F);
+	
+	OLED_WriteCommand(0xD3);	//设置显示偏移
+	OLED_WriteCommand(0x00);
+	
+	OLED_WriteCommand(0x40);	//设置显示开始行
+	
+	OLED_WriteCommand(0xA1);	//设置左右方向，0xA1正常 0xA0左右反置
+	
+	OLED_WriteCommand(0xC8);	//设置上下方向，0xC8正常 0xC0上下反置
+
+	OLED_WriteCommand(0xDA);	//设置COM引脚硬件配置
+	OLED_WriteCommand(0x12);
+	
+	OLED_WriteCommand(0x81);	//设置对比度控制
+	OLED_WriteCommand(0xCF);
+
+	OLED_WriteCommand(0xD9);	//设置预充电周期
+	OLED_WriteCommand(0xF1);
+
+	OLED_WriteCommand(0xDB);	//设置VCOMH取消选择级别
+	OLED_WriteCommand(0x30);
+
+	OLED_WriteCommand(0xA4);	//设置整个显示打开/关闭
+
+	OLED_WriteCommand(0xA6);	//设置正常/倒转显示
+
+	OLED_WriteCommand(0x8D);	//设置充电泵
+	OLED_WriteCommand(0x14);
+
+	OLED_WriteCommand(0xAF);	//开启显示
+		
+	OLED_Clear();				//OLED清屏
 }
 
 int
 main(void) {
 
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();	// Blue pill
-	//rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
-	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_GPIOB);
-	rcc_periph_clock_enable(RCC_GPIOC);
-	rcc_periph_clock_enable(RCC_AFIO);
-	rcc_periph_clock_enable(RCC_SPI1);
 
-	// LED on PC13
-	gpio_set_mode(GPIOC,
-		GPIO_MODE_OUTPUT_2_MHZ,
-		GPIO_CNF_OUTPUT_PUSHPULL,
-		GPIO13);
-	gpio_set(GPIOC,GPIO13);	// PC13 = on
+	OLED_Init();
 
-	// Put SPI1 on PB5/PB4/PB3/PA15
-        gpio_primary_remap(
-                AFIO_MAPR_SWJ_CFG_JTAG_OFF_SW_OFF, // Optional
-		AFIO_MAPR_SPI1_REMAP);
+	OLED_ShowChar(1, 1, 'A');
 
-	// PB10 -> D/C, PB11 -> RES
-	gpio_set_mode(GPIOB,
-		GPIO_MODE_OUTPUT_2_MHZ,
-		GPIO_CNF_OUTPUT_PUSHPULL,
-		GPIO10|GPIO11);
-	// Activate OLED Reset line
-	gpio_clear(GPIOB,GPIO11); 
-
-	// PB5=MOSI, PB3=SCK
-	gpio_set_mode(
-		GPIOB,
-                GPIO_MODE_OUTPUT_50_MHZ,
-        	GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                GPIO5|GPIO3
-	);
-	// NSS=PA15
-	gpio_set_mode(
-		GPIOA,
-                GPIO_MODE_OUTPUT_50_MHZ,
-        	GPIO_CNF_OUTPUT_ALTFN_PUSHPULL,
-                GPIO15
-	);
-
-	//spi_reset(SPI1); 
-	rcc_periph_reset_pulse(RST_SPI1);
-	spi_init_master(
-		SPI1,
-                SPI_CR1_BAUDRATE_FPCLK_DIV_256,
-                SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
-		SPI_CR1_CPHA_CLK_TRANSITION_1,
-	        SPI_CR1_DFF_8BIT,
-	        SPI_CR1_MSBFIRST
-	);
-	spi_disable_software_slave_management(SPI1);
-	spi_enable_ss_output(SPI1);
-
-	usb_start(1,1);
-	std_set_device(mcu_usb);			// Use USB for std I/O
-	gpio_clear(GPIOC,GPIO13);			// PC13 = off
-
-	xTaskCreate(monitor_task,"monitor",500,NULL,1,NULL);
-	vTaskStartScheduler();
+	// xTaskCreate(monitor_task,"monitor",500,NULL,1,NULL);
+	// vTaskStartScheduler();
 	for (;;);
 	return 0;
 }
