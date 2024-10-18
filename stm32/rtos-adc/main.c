6c2266d17f443400b1b464d0d3f3ce27c4a2134d
mrzhuzhe <466648706@qq.com>
2024-10-18 23:21:38 +0800

    scan mode

--- stm32/rtos-adc/main.c
+++ stm32/rtos-adc/main.c
@@ -385,21 +385,19 @@ main(void) {
 	gpio_set_mode(GPIOA,
 		GPIO_MODE_INPUT,
 		GPIO_CNF_INPUT_ANALOG,
-		GPIO3);				// PA0 & PA1
+		GPIO3|GPIO2);				// PA0 & PA1
 
 	// Initialize ADC:
 	rcc_peripheral_enable_clock(&RCC_APB2ENR,RCC_APB2ENR_ADC1EN);
 	adc_power_off(ADC1);
 	rcc_peripheral_reset(&RCC_APB2RSTR,RCC_APB2RSTR_ADC1RST);
 	rcc_peripheral_clear_reset(&RCC_APB2RSTR,RCC_APB2RSTR_ADC1RST);
-	//rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV6);	// Set. 12MHz, Max. 14MHz
+
 	adc_set_dual_mode(ADC_CR1_DUALMOD_IND);		// Independent mode
-	adc_disable_scan_mode(ADC1);
+	adc_enable_scan_mode(ADC1);
 	adc_set_right_aligned(ADC1);
 	adc_set_single_conversion_mode(ADC1);
-	//adc_set_sample_time(ADC1,ADC_CHANNEL_TEMP,ADC_SMPR_SMP_239DOT5CYC);
-	//adc_set_sample_time(ADC1,ADC_CHANNEL_VREF,ADC_SMPR_SMP_55DOT5CYC);
-	//adc_enable_temperature_sensor();
+
 	adc_power_on(ADC1);
 	adc_reset_calibration(ADC1);
 	adc_calibrate_async(ADC1);
