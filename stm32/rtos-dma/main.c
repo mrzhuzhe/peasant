bba6877e491c63ac8010d85f9bb4f1d818c60be4
mrzhuzhe <466648706@qq.com>
2024-10-19 00:44:33 +0800

    weild

--- stm32/rtos-dma/main.c
+++ stm32/rtos-dma/main.c
@@ -360,74 +360,150 @@ demo_task(void *arg __attribute((unused))) {
 	}
 }
 
+
+
+enum { CH2, CH3  };
+
+// Static allocations
+const uint8_t channel_lenght = 2;
+uint16_t channels[2];
+uint8_t chanlist[2] = { 2, 3 };
+volatile bool flag;
+
 // ISR
 void dma1_channel1_isr()
 {
+	flag = true;
+
 	if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL1, DMA_TCIF))
 		dma_clear_interrupt_flags(DMA1, DMA_CHANNEL1, DMA_TCIF);
 }
 
-int
-main(void) {
-	uint16_t AD_Value[4];
-	
+void clock_setup()
+{
+	// high-speed external oscillator (HSE) at 8MHz.
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+	rcc_periph_clock_enable(RCC_GPIOA); // PA6
+	rcc_periph_clock_enable(RCC_USART1);
+}
+
+void dma_setup()
+{
+	// enable IRQ
+	// Without this the timer interrupt routine will never be called.
 	nvic_enable_irq(NVIC_DMA1_CHANNEL1_IRQ);
 
-	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+	// configure the DMA
 	rcc_periph_clock_enable(RCC_DMA1);
 
-	OLED_Init();
+	// The channel is disabled and configuration registers are cleared.
+	dma_channel_reset(DMA1, DMA_CHANNEL1);
 
-	rcc_periph_clock_enable(RCC_GPIOA);		// Enable GPIOA for ADC
-	gpio_set_mode(GPIOA,
-		GPIO_MODE_INPUT,
-		GPIO_CNF_INPUT_ANALOG,
-		GPIO3|GPIO2);				// PA0 & PA1
+	// Priority.
+	//dma_set_priority(DMA1, DMA_CHANNEL1, DMA_CCR_PL_VERY_HIGH);
+	dma_set_priority(DMA1, DMA_CHANNEL1, DMA_CCR_PL_LOW);
 
-	// Initialize ADC:
-	rcc_peripheral_enable_clock(&RCC_APB2ENR,RCC_APB2ENR_ADC1EN);
-	adc_power_off(ADC1);
-	rcc_peripheral_reset(&RCC_APB2RSTR,RCC_APB2RSTR_ADC1RST);
-	rcc_peripheral_clear_reset(&RCC_APB2RSTR,RCC_APB2RSTR_ADC1RST);
-	
-	/* Prescaler */
-	rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV6);
-	
-	// adc_set_dual_mode(ADC_CR1_DUALMOD_IND);		// Independent mode
-	adc_enable_scan_mode(ADC1);
-	adc_set_right_aligned(ADC1);
+	/* 16Bit wide transfer for source and destination. */
+	// dma_set_memory_size(DMA1, DMA_CHANNEL1, DMA_CCR_MSIZE_32BIT);
+	// dma_set_peripheral_size(DMA1, DMA_CHANNEL1, DMA_CCR_PSIZE_32BIT);
+	dma_set_memory_size(DMA1, DMA_CHANNEL1, DMA_CCR_MSIZE_16BIT);
+	dma_set_peripheral_size(DMA1, DMA_CHANNEL1, DMA_CCR_PSIZE_16BIT);
 
-	// adc_set_sample_time(ADC1,2,ADC_SMPR_SMP_55DOT5CYC);
-	// adc_set_sample_time(ADC1,3,ADC_SMPR_SMP_55DOT5CYC);
-	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_55DOT5CYC);
-	uint8_t chanels[] = {2,3};
-	adc_set_regular_sequence(ADC1,2, chanels);
 	/*
 	 * After every 32bits we have to increase the address because
 	 * we use RAM.
 	 */
+	// dma_enable_peripheral_increment_mode(DMA1, DMA_CHANNEL1);
+	//dma_disable_peripheral_increment_mode(DMA1, DMA_CHANNEL1); // should be default
 	dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL1);
-	dma_disable_peripheral_increment_mode(DMA1, DMA_CHANNEL1);
+	dma_enable_circular_mode(DMA1, DMA_CHANNEL1);
 
-	dma_channel_reset(DMA1,DMA_CHANNEL1);
-	dma_set_peripheral_address(DMA1,DMA_CHANNEL1,(uint32_t)&ADC1_DR);
-	dma_set_peripheral_size(DMA1,DMA_CHANNEL1,DMA_CCR_PSIZE_16BIT);
+	/* We define the source as peripheral. */
+	dma_set_read_from_peripheral(DMA1, DMA_CHANNEL1);
 
-	dma_set_memory_size(DMA1,DMA_CHANNEL1,DMA_CCR_MSIZE_16BIT);
-	dma_set_priority(DMA1,DMA_CHANNEL1,DMA_CCR_PL_MEDIUM);
-	dma_enable_circular_mode(DMA1,DMA_CHANNEL1);
+	/* We want to transfer ADC Data Register. */
+	dma_set_peripheral_address(DMA1, DMA_CHANNEL1, (uint32_t)&ADC1_DR);
 
-	//dma_disable_channel(DMA1,DMA_CHANNEL1);
-	dma_set_memory_address(DMA1,DMA_CHANNEL1,(uint32_t)AD_Value);
-	dma_set_number_of_data(DMA1,DMA_CHANNEL1, 4);
-	dma_enable_channel(DMA1,DMA_CHANNEL1);
+	/* Destination should be the channels array. */
+	dma_set_memory_address(DMA1, DMA_CHANNEL1, (uint32_t)channels);
+
+	/*
+	 * Set number of DATA to transfer.
+	 * Remember that this means not necessary bytes but MSIZE or PSIZE
+	 * depending on your source device.
+	 */
+	dma_set_number_of_data(DMA1, DMA_CHANNEL1, channel_lenght);
+
+	dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL1);
+
+	/* Start DMA transfer. */
+	dma_enable_channel(DMA1, DMA_CHANNEL1);
+}
+
+/**
+ * Initialize the ADC1.
+ *
+ * @warning: rcc_periph_clock_enable(RCC_GPIOA);
+ */
+void adc_setup()
+{
+	gpio_set_mode(GPIOA,
+			GPIO_MODE_INPUT,
+			GPIO_CNF_INPUT_ANALOG,
+			GPIO2 | GPIO3);
+
+	rcc_periph_clock_enable(RCC_ADC1);
+	adc_power_off(ADC1); // Make sure the ADC doesn't run during config.
+
+	dma_setup();
+
+	/* rcc_peripheral_reset + rcc_peripheral_clear_reset */
+	rcc_periph_reset_pulse(RST_ADC1);
+
+	/* Prescaler */
+	rcc_set_adcpre(RCC_CFGR_ADCPRE_PCLK2_DIV6);
+
+	/* Independent mode */
+	// adc_set_dual_mode(ADC_CR1_DUALMOD_IND);
+
+	/* Scan mode */
+	// adc_disable_scan_mode(ADC1); // convert only 1 channel
+	adc_enable_scan_mode(ADC1); // scan channels mode (DMA required)
+
+	/* configure for one single conversion. */
+	adc_set_single_conversion_mode(ADC1);
+
+	// F1 software trigger mandatory
+	adc_enable_external_trigger_regular(ADC1, ADC_CR2_EXTSEL_SWSTART);
+	// adc_disable_external_trigger_regular(ADC1);
+
+	/* alignment */
+	adc_set_right_aligned(ADC1);
+
+	// adc_set_sample_time(ADC1, ADC_CHANNEL6, ADC_SMPR_SMP_239DOT5CYC);
+	adc_set_sample_time_on_all_channels(ADC1, ADC_SMPR_SMP_239DOT5CYC);
+
+	adc_set_regular_sequence(ADC1, channel_lenght, (uint8_t*)chanlist);
 
 	adc_enable_dma(ADC1);
-
 	adc_power_on(ADC1);
 	adc_reset_calibration(ADC1);
-	adc_calibrate_async(ADC1);
-	while ( adc_is_calibrating(ADC1) );
+	adc_calibrate(ADC1);
+}
+
+
+int
+main(void) {
+	
+	nvic_enable_irq(NVIC_DMA1_CHANNEL1_IRQ);
+
+
+	OLED_Init();
+	clock_setup();
+	adc_setup();
+	
+
+	
 
 	OLED_ShowChar(1, 1, 'A');
 	OLED_ShowString(1, 3, "Fuck you!");
@@ -444,8 +520,8 @@ main(void) {
 		// OLED_ShowNum(2, 6, adc2, 5);
 		// OLED_ShowNum(3, 6, adc3, 5);
 		adc_start_conversion_regular(ADC1);
-		OLED_ShowNum(2, 6, AD_Value[0], 5);
-		OLED_ShowNum(3, 6, AD_Value[1], 5);
+		OLED_ShowNum(2, 6, channels[CH2], 5);
+		OLED_ShowNum(3, 6, channels[CH3], 5);
 	}
 
 	// rtos cannot go with OLED
