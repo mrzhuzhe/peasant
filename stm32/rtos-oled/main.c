98a2d3b32aae70aa1fe9084e728284c12dd9d4e4
mrzhuzhe <466648706@qq.com>
2024-10-19 23:24:09 +0800

    Os

--- stm32/rtos-oled/main.c
+++ stm32/rtos-oled/main.c
@@ -324,27 +324,19 @@ void OLED_Init(void)
 	OLED_Clear();				//OLED清屏
 }
 
-static SemaphoreHandle_t sem_flash = 0;
-
 void
 monitor_task(void *arg) {
 
 	for (;;) {
-		//xSemaphoreTake(sem_flash,portMAX_DELAY);
-		
-
-		// OLED_ShowChar(1, 1, 'A');
-		// OLED_ShowString(1, 3, "Fuck you!");
-		// OLED_ShowNum(2, 1, 12345, 5);
-		// OLED_ShowSignedNum(2, 7, -66, 4);
-		// OLED_ShowHexNum(3, 1, 0xAA55, 4);
-		// OLED_ShowBinNum(4, 1, 0xAA55, 16);
+		OLED_ShowChar(1, 1, 'A');
+		OLED_ShowString(1, 3, "Fuck you!");
+		OLED_ShowNum(2, 1, 12345, 5);
+		OLED_ShowSignedNum(2, 7, -66, 4);
+		OLED_ShowHexNum(3, 1, 0xAA55, 4);
+		OLED_ShowBinNum(4, 1, 0xAA55, 16);
 		
 		gpio_toggle(GPIOC,GPIO14);
 		vTaskDelay(pdMS_TO_TICKS(500));
-
-		//xSemaphoreGive(sem_flash);
-		//vTaskDelay(pdMS_TO_TICKS(400));
 	}
 }
 
@@ -354,7 +346,7 @@ main(void) {
 	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
 
 
-	//OLED_Init();
+	OLED_Init();
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO14);
 	gpio_set(GPIOC,GPIO14);
