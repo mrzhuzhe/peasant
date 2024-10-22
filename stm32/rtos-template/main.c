a6c86824d346530bf5efdf9222385ad20936be50
mrzhuzhe <466648706@qq.com>
2024-10-22 21:52:39 +0800

    oled

--- stm32/rtos-template/main.c
+++ stm32/rtos-template/main.c
@@ -10,7 +10,7 @@
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-
+#include "oled.h"
 
 // extern void vApplicationStackOverflowHook(TaskHandle_t pxTask, portCHAR *pcTaskName);
 
@@ -37,9 +37,17 @@ task1(void *args) {
 		// //vTaskDelay(pdMS_TO_TICKS(500));
 		// gpio_set(GPIOC,GPIO14);
 		// for (i = 0; i < 5e5; i++)
-		// 	__asm__("nop");	
+		// 	__asm__("nop");			
 		gpio_toggle(GPIOC,GPIO14);
 		vTaskDelay(pdMS_TO_TICKS(500));
+		
+		OLED_ShowChar(1, 1, 'A');
+		OLED_ShowString(1, 3, "Fuck you!");
+		OLED_ShowNum(2, 1, 12345, 5);
+		OLED_ShowSignedNum(2, 7, -66, 4);
+		OLED_ShowHexNum(3, 1, 0xAA55, 4);
+		OLED_ShowBinNum(4, 1, 0xAA55, 16);
+	
 	}
 }
 
@@ -52,6 +60,8 @@ main(void) {
 	rcc_periph_clock_enable(RCC_GPIOC);
 	gpio_set_mode(GPIOC,GPIO_MODE_OUTPUT_2_MHZ,GPIO_CNF_OUTPUT_PUSHPULL,GPIO14);
 	gpio_set(GPIOC,GPIO14);
+
+	OLED_Init();
 
 	xTaskCreate(task1,"LED",100,NULL,configMAX_PRIORITIES-1,NULL);
 	vTaskStartScheduler();
