bd77b911b42b4f55cdd60e910d7c3d4de45af666
mrzhuzhe <466648706@qq.com>
2025-03-15 21:27:19 +0800

    epool test

--- server-test/epool/main.c
+++ server-test/epool/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+#include <unistd.h>
+#include <sys/epoll.h>
+#include <string.h>   
+
 int main(){
     printf("test epoll\n");
+
+    int epoll_fd = epoll_create1(0);
+	
+	if (epoll_fd == -1) {
+		fprintf(stderr, "Failed to create epoll file descriptor\n");
+		return 1;
+	}
+
+	if (close(epoll_fd)) {
+		fprintf(stderr, "Failed to close epoll file descriptor\n");
+		return 1;
+	}
+    
     return 0;
 }
