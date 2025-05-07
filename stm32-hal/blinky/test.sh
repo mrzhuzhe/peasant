0927cf45ad97f513d203e9d4a40ba1049e1a7ed4
mrzhuzhe <466648706@qq.com>
2025-05-07 22:07:38 +0800

    dot graph

--- stm32-hal/blinky/test.sh
+++ stm32-hal/blinky/test.sh
@@ -2,5 +2,9 @@ export STM32_TOOLCHAIN_PATH="$HOME/opt/cross-arm"
 export STM32_TARGET_TRIPLET=arm-none-eabi
 export STM32_CUBE_F1_PATH="/home/sanszhu/code/code/STM32CubeF1"
 
-cmake -B build -S .
-cmake --build build
+cmake -B build -S . --graphviz=foo.dot
+cmake --build build 
+
+# dot -Tpng -o ./outputs/foo.png ./outputs/foo.dot
+# dot -Tpng -o ./outputs/foo.dot.CMSISSTM32F1.dependers.png ./outputs/foo.dot.CMSISSTM32F1.dependers
+# dot -Tpng -o ./outputs/foo.dot.STM32F1.dependers.png ./outputs/foo.dot.STM32F1.dependers
