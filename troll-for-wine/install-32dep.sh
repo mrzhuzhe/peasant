0a3b4d170fa3870828f4bc3b095fc06b4eae05cf
mrzhuzhe <466648706@qq.com>
2025-06-02 00:03:26 +0800

    depdents hell

---
+++ troll-for-wine/install-32dep.sh
@@ -0,0 +1,7 @@
+apt install libxrender-dev:i386
+apt install apt install libasound2-dev:i386
+apt install libgl-dev:i386
+apt install libgnutls28-dev:i386
+apt install libgstreamer1.0-dev:i386
+apt install libxrandr-dev:i386
+apt install libxcomposite-dev:i386 
