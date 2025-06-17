9de09895bc7f43f1f41c5db0008ef32219b35359
mrzhuzhe <466648706@qq.com>
2025-06-17 23:28:05 +0800

    gcc int

--- llvm/cpu0_test/main.c
+++ llvm/cpu0_test/main.c
@@ -3,6 +3,7 @@
     CC = clang++ -stdlib=libstdc++ -std=c++14
     1. seems lllvm-project not compile libc++ yet
     2. seems no lld, gcc ld need specific llvm libs 
+    3. #CC = llc -debug-pass=Structure
 */
   
 
