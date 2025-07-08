ad18170be90947f5c4cc03d99e77b8955c1530e7
mrzhuzhe <466648706@qq.com>
2025-07-08 22:19:34 +0800

    remove ast

--- llvm/ast/passes/pass.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "kaleidoscope/kaleidoscope.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/LegacyPassManager.h"
-
-void InitializeModuleAndPassManagers();
