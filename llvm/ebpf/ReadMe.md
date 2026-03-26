56f160e5b35e10d1cc3dd96cfcb5eaf11011b694
mrzhuzhe <466648706@qq.com>
2026-03-26 19:32:08 +0800

    ffmpeg

--- llvm/ebpf/ReadMe.md
+++ llvm/ebpf/ReadMe.md
@@ -1,2 +1,2 @@
 1. user space and kernel space https://eunomia.dev/tutorials/11-bootstrap/
-2. https://docs.ebpf.io/concepts/core/
+2. https://docs.ebpf.io/concepts/core/ 
---
+++ llvm/ebpf/test/main.c
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#define BPF_NO_GLOBAL_DATA
+// #include "vmlinux.h"
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+typedef unsigned int u32;
+typedef int pid_t;
+const pid_t pid_filter = 0;
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+// this is not work yet maybe because sec target is not 
+SEC("tp/syscalls/sys_enter_write")
+int handle_tp(void *ctx)
+{
+ pid_t pid = bpf_get_current_pid_tgid() >> 32;
+ if (pid_filter && pid != pid_filter)
+  return 0;
+ bpf_printk("BPF triggered sys_enter_write from PID %d.\n", pid);
+ return 0;
+}
+
+// echo 1 > /sys/kernel/tracing/tracing_on Noticd cannot trigger this trace
+// cat /sys/kernel/tracing/trace_pipe 
