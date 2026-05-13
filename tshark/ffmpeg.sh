d7b6252d4a1a1a9570ad732f63b67e29343ac695
mrzhuzhe <466648706@qq.com>
2026-05-13 21:09:44 +0800

    kvm cs

--- tshark/ffmpeg.sh
+++ tshark/ffmpeg.sh
@@ -1,4 +1,4 @@
 # this is test for reading m3u8 url
 # but youtube and bilibili video cannot find u3m8 url directly
-ffmpeg -i "https://s5.sanheyiliao.xyz//videos/202604/11/69d934d4ce6d9f98982d4a14/7f3464/index.m3u8" -c copy test.mkv
+ffmpeg -i "https://tx-safety-video.acfun.cn/mediacloud/acfun/acfun_video/hls/134373999005147136_HLS_720P_1.m3u8?pkey=ABB1578urHIwRjG-k6SJoRVHimeBcTtXA8CBpy8dGkzaJXtyaX8wKmnUqglob6daCoYOrFxidT2tktdwULk2w_co9bRST07T9druu4m1g4UtjJSm5RoXyZnXM0nInvJsbk3QDx-BMV28yalKL_TAa5oN20KKedvsB50e-txwN6Mo7EQnBgPl6nJVWAj8nKr1Jwm2beXPh-AuCZtZxD-F1IexT6GjTo7epaeEp0gfr7--kd1ZdRwR94KWZZTxt52Ms9c&safety_id=AAKm9HyTLoJNUcWCXAqslGU8" -c copy test.mkv
 ffmpeg -i "outputs/test.mkv" -codec copy "outputs/test.mp4"
---
+++ vmx/ReadMe.md
@@ -0,0 +1,9 @@
+##
+1. intel vtx control structure, APIC, MTRR CR
+
+## Refference 
+
+1. https://github.com/dpw/kvm-hello-world
+2. https://david942j.blogspot.com/2018/10/note-learning-kvm-implement-your-own.html
+3. https://software.intel.com/sites/manageability/AMT_Implementation_and_Reference_Guide/default.htm?turl=WordDocuments%2Fkvmapplicationdevelopersguide.htm
+4. https://docs.kernel.org/virt/kvm/api.html
